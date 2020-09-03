4c66e3bff5826c520edd53d8ab269db1b1fc6649
Sega <057sega057@gmail.com>
2020-09-03 12:18:23 +0300

    fix request url

--- src/app/utils/get-request-url.ts
+++ src/app/utils/get-request-url.ts
@@ -3,7 +3,5 @@ export function getRequestUrl(): URL {
         return new URL(process.env.BASE_URL + "");
     }
 
-    const requestUrl = new URL(window.location.origin);
-    requestUrl.hostname = "pay." + requestUrl.hostname;
-    return requestUrl;
+    return  new URL(window.location.origin.replace("pay.", ""));
 }
