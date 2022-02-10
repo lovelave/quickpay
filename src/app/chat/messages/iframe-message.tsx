0d95927dba6bb62897cdc8e67f60c6d2c6ec8e59
horat1us <reclamme@gmail.com>
2022-02-10 16:04:04 +0200

    Remove Platon IFrame (replace with _black form)

--- src/app/chat/messages/iframe-message.tsx
+++ src/app/chat/messages/iframe-message.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as Chat from "../chat-logic";
 import * as Base from "../base";
-import {IframePayment} from "./iframe-payment";
+import {IframePaymentForm} from "./iframe-payment";
 
-export const IframeMessage: React.FC<{value: Chat.IframeMessage}> = ({value: {action, data}}) => {
+export const IframeMessage: React.FC<{ value: Chat.IframeMessage }> = ({value: {action, data}}) => {
     const dispatch = Chat.useDispatchContext();
 
     const handleComplete = React.useCallback(() => {
@@ -19,8 +19,12 @@ export const IframeMessage: React.FC<{value: Chat.IframeMessage}> = ({value: {ac
     }, [dispatch]);
 
     return (
-        <Base.WrapMessage source="user" className="message-card">
-            <IframePayment onComplete={handleComplete} onError={handleError} data={data} action={action}/>
-        </Base.WrapMessage>
+        <Base.ButtonMessage source="user" className="message-card">
+            <IframePaymentForm data={data} action={action} target="_blank">
+                <Base.Button theme={"secondary"} type={"submit"}>
+                    Оплатить
+                </Base.Button>
+            </IframePaymentForm>
+        </Base.ButtonMessage>
     );
 }
